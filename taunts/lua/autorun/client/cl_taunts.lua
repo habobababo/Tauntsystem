d23000d13994cd87e50964993e7a0915d98b20e5
habobababo <shadowman11221@gmail.com>
2016-04-17 12:23:35 +0200

    Update cl_taunts.lua
    fixed something weird

--- taunts/lua/autorun/client/cl_taunts.lua
+++ taunts/lua/autorun/client/cl_taunts.lua
@@ -4,8 +4,10 @@ net.Receive( "CoreTauntAct", function()
 	local id = net.ReadString()
 	for k,v in pairs(player.GetAll()) do
 		if v:SteamID64() == id then
-			if table.HasValue(CoreTaunts.AllowedGroupsD, v:GetNWString("usergroup")) then
-				v:AnimRestartGesture(GESTURE_SLOT_ATTACK_AND_RELOAD, act, true)	
+			if CoreTaunts.CanDance then
+				if table.HasValue(CoreTaunts.AllowedGroupsD, v:GetNWString("usergroup")) then
+					v:AnimRestartGesture(GESTURE_SLOT_ATTACK_AND_RELOAD, act, true)	
+				end
 			end
 		end
 	end
