4b91e0ced42a2e908162d875688686787f615172
habobababo <shadowman11221@gmail.com>
2016-04-17 12:20:19 +0200

    Update cl_taunts.lua
    added the groups allowed to use

--- taunts/lua/autorun/client/cl_taunts.lua
+++ taunts/lua/autorun/client/cl_taunts.lua
@@ -1,11 +1,12 @@
 
-
 net.Receive( "CoreTauntAct", function()
 	local act = table.Random(CoreTaunts.Acts)
 	local id = net.ReadString()
 	for k,v in pairs(player.GetAll()) do
 		if v:SteamID64() == id then
-			v:AnimRestartGesture(GESTURE_SLOT_ATTACK_AND_RELOAD, act, true)	
+			if table.HasValue(CoreTaunts.AllowedGroupsD, v:GetNWString("usergroup")) then
+				v:AnimRestartGesture(GESTURE_SLOT_ATTACK_AND_RELOAD, act, true)	
+			end
 		end
 	end
 end)
